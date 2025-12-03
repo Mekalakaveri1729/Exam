a987a042ef3245820ced12bb422e4ffbcb1d42c0
Mekalakaveri1729 <146640636+Mekalakaveri1729@users.noreply.github.com>
2025-12-03 13:53:27 +0530

    Update test_case.py

--- tests/test_case.py
+++ tests/test_case.py
@@ -8,7 +8,7 @@ import time
 @pytest.fixture
 def setup_teardown():
     driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("http://127.0.0.1:5000")
+    driver.get("https://localhost:5000")
     yield driver
     driver.quit()
 def get_alert_text(driver):
@@ -18,7 +18,7 @@ def get_alert_text(driver):
     return text    
 def test_empty_username(setup_teardown):
     driver=setup_teardown
-    driver.get("http://127.0.0.1:5000")
+    driver.get("https://localhost:5000")
     driver.find_element(By.NAME,"username").clear()
     driver.find_element(By.NAME,"password").send_keys("Kaveri@1729")
     driver.find_element(By.NAME,"sb").click()
@@ -27,7 +27,7 @@ def test_empty_username(setup_teardown):
     assert alert_text=="Username cannot be empty"
 def test_empty_password(setup_teardown):
     driver=setup_teardown
-    driver.get("http://127.0.0.1:5000")
+    driver.get("https://localhost:5000")
     driver.find_element(By.NAME,"username").send_keys("Kaveri")
     driver.find_element(By.NAME,"password").clear() 
     driver.find_element(By.NAME,"sb").click()
@@ -36,7 +36,7 @@ def test_empty_password(setup_teardown):
     assert alert_text=="Password cannot be empty"
 def test_short_pwd(setup_teardown):
     driver=setup_teardown
-    driver.get("http:/127.0.0.1.:5000")
+    driver.get("https://localhost:5000")
     driver.find_element(By.NAME,"username").send_keys("Kaveri")
     driver.find_element(By.NAME,"password").send_keys("Kave")
     driver.find_element(By.NAME,"sb").click()
@@ -45,7 +45,7 @@ def test_short_pwd(setup_teardown):
     assert alert_text=="Password must be at least 6 characters long"
 def test_valid_input(setup_teardown):
     driver=setup_teardown
-    driver.get("http://127.0.0.1:5000")
+    driver.get("https://localhost:5000")
     driver.find_element(By.NAME,"username").send_keys("Kaveri")
     driver.find_element(By.NAME,"password").send_keys("Kaveri@1729")
     driver.find_element(By.NAME,"sb").click()
@@ -54,5 +54,6 @@ def test_valid_input(setup_teardown):
     assert "/submit" in current_url,f'expected greeting page but find {current_url}'
     body_text=driver.fibd_element(By.TAG_NAME,"body").text
     assert "Hello,Kaveri!Welcome to the Website" in body_text,f'expected not matched with {body_text}'
+
 
 
